32d7d5b2d9466630de7085c5622d6f9bbc2de20f
lingmin-tech <lingmin893@126.com>
2026-05-31 22:59:37 +0800

    init: first commit

---
+++ src/main/java/com/neo/neocloud/NeoCloudApplication.java
@@ -0,0 +1,13 @@
+package com.neo.neocloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoCloudApplication.class, args);
+	}
+
+}
